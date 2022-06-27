f18fdae25a6e2bd110d3bd8c6dd8a24f848f4feb
moeketsi <raymondkxng17@gmail.com>
2022-06-27 13:43:53 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -20,22 +20,52 @@ const App = () => {
                             </button>
                         </TooltipComponent>
                     </div>
-                    {activeMenu ? (
-                        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-                            Sidebar
-                        </div>
-                    ):(
-                        <div className='w-0 dark:bg-secondary-dark-bg'>
-                            Sidebar 
-                        </div>
-                    )}
-                    <div className={ 
-                        `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
-                    }>
+                        {activeMenu ? (
+                            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                                Sidebar
+                            </div>
+                        ):(
+                            <div className='w-0 dark:bg-secondary-dark-bg'>
+                                Sidebar 
+                            </div>
+                        )}
+                        <div className={ 
+                            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                        }>
                         <div className='fixed md:static bg-main-bg dark:bg-main-bg navbar w-full'>
                             navbar
                         </div>
                     </div>
+                    
+                    <div className=''>
+                        <Routes>
+                            {/* dashboard */}
+                            <Route path='/' element='ECommerce'/>
+                            <Route path='/ecommerece' element='ECommerce'/>
+
+                            {/* pages */}
+                            <Route path='/orders' element='Orders' />
+                            <Route path='/employees' element='Employees' />
+                            <Route path='/customers' element='Customers' />
+
+                            {/* apps */}
+                            <Route path='/kanban' element='Kanban' />
+                            <Route path='/editor' element='Editor' />
+                            <Route path='/calendar' element='Calendar' />
+                            <Route path='/color-picker' element='ColorPicker' />
+
+                            {/* charts */}
+                            <Route path='/line' element='Line' />
+                            <Route path='/area' element='Area' />
+                            <Route path='/bar' element='Bar' />
+                            <Route path='/pie' element='Pie' />
+                            <Route path='/financial' element='financial' />
+                            <Route path='/color-mapping' element='ColorMapping' />
+                            <Route path='/pyramid' element='Pyramid' />
+                            <Route path='/stacked' element='Stacked' />
+                        </Routes>
+                    </div>
+
                 </div>
             </BrowserRouter>
         </div>
