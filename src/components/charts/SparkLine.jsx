46f72e5568c3f70d5ec641c98579b7a82d63a2c6
moeketsi <raymondkxng17@gmail.com>
2022-07-01 16:11:52 +0200

    commit

--- src/components/charts/SparkLine.jsx
+++ src/components/charts/SparkLine.jsx
@@ -3,22 +3,31 @@ import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-re
 
 const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
     return (
-        <SparklineComponent
-            id={id}
-            height={height}
-            width={width}
-            lineWidth={1}
-            valueType='Numeric'
-            fill={color}
-            border={{ color: currentColor, width: 2 }}
-            dataSource={data}
-            xName='x'
-            yName='y'
-            type={type}
-            tooltipSetting={{visible: true, format: '${x} : data ${y}', trackLineSettings: { visible: true}}}
-        >
-            <Inject services={[SparklineTooltip]} />
-        </SparklineComponent>
+        // <SparklineComponent
+        //     id={id}
+        //     height={height}
+        //     width={width}
+        //     lineWidth={1}
+        //     valueType="Numeric"
+        //     fill={color}
+        //     border={{ color: currentColor, width: 2 }}
+        //     tooltipSettings={{
+        //         visible: true,
+        //         format: '${x} : data ${yval}',
+        //         trackLineSettings: {
+        //             visible: true,
+        //         },
+        //     }}
+        //     markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        //     legensSettings={{background: 'white'}}
+        //     dataSource={data}
+        //     xName="x"
+        //     yName="yval"
+        //     type={type}
+        // >
+        //     <Inject services={[SparklineTooltip]} />
+        // </SparklineComponent>
+        <div></div>
     )
 }
 
