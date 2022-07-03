f2852c43d55c8a4cb1f5f2615b227dca33e8c321
moeketsi <raymondkxng17@gmail.com>
2022-07-03 15:04:47 +0200

    commit kanban

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -7,17 +7,17 @@ const Kanban = () => {
     return (
         <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl' >
             <Header category='App' title='Kanban' />
+
             <KanbanComponent
-                id='kanban'
+                id="kanban"
+                keyField="Status"
                 dataSource={kanbanData}
-                cardSettings={{ contentFeild: 'Summary', headerField: 'Id' }}
-                keyField='Status'
+                cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
             >
                 <ColumnsDirective>
                     {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
                 </ColumnsDirective>
             </KanbanComponent>
-            
         </div>
     )
 }
